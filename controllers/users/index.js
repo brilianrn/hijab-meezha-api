65d69b3672f7f3cc35263f22972b2220a3488616
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2022-08-10 21:12:39 +0700

    feat: [User] Forgot password

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,6 +1,13 @@
 const { CustomerLogin } = require('./login');
 const { CustomerRegister } = require('./register');
+const { ResetPassword } = require('./reset-password');
 const { UserInfo } = require('./user-info');
 const { FogotUseEmail } = require('./forgot-password');
 
-module.exports = { CustomerLogin, CustomerRegister, FogotUseEmail, UserInfo };
+module.exports = {
+  CustomerLogin,
+  CustomerRegister,
+  FogotUseEmail,
+  UserInfo,
+  ResetPassword,
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,11 +4,13 @@ const {
   CustomerLogin,
   UserInfo,
   FogotUseEmail,
+  ResetPassword,
 } = require('../controllers/users');
 const { authentication } = require('../middlewares/authorization');
 
 route.post('/register', CustomerRegister);
 route.post('/login', CustomerLogin);
+route.post('/reset-password/:access_token', ResetPassword);
 route.post('/forgot-password-use-email', FogotUseEmail);
 
 route.get('/info', authentication, UserInfo);
