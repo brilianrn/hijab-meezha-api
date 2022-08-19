9032d3fd42d5938ae72912b1a8adf6dc78c543d2
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2022-08-19 16:16:17 +0700

    feat: [Products] Product size done

---
+++ controllers/products/sizes/create.js
@@ -0,0 +1,65 @@
+const { errors, successMessageTypes } = require('../../../constants');
+const { Size, Category } = require('../../../models');
+const formatResponse = require('../../../utils/format-response');
+const { successMessages } = require('../../../utils/messages-generate');
+const { UuidCheck } = require('../../../utils/uuid-check');
+
+const CreateProductSize = async (req, res, next) => {
+  const { name, categoryId } = req.body;
+  const { id } = req.currentAdmin;
+
+  if (!name)
+    return next({ name: errors['400_EMPTY_FIELD'], description: 'Size name' });
+  if (!categoryId)
+    return next({
+      name: errors['400_EMPTY_FIELD'],
+      description: 'Category ID',
+    });
+  if (!UuidCheck(categoryId))
+    return next({
+      name: errors['400_WRONG_FIELD'],
+      description: 'Category ID',
+    });
+
+  try {
+    const category = await Category.findOne({ where: { id: categoryId } });
+    if (!category) {
+      return next({
+        name: errors['400_NOT_EXIST'],
+        description: 'Category ID',
+      });
+    }
+
+    const checkSize = await Size.findOne({ where: { name, categoryId } });
+    if (checkSize) {
+      if (checkSize.name === name && checkSize.categoryId === categoryId) {
+        return next({
+          name: errors['400_EXIST_DATA'],
+          description: 'Size',
+        });
+      }
+    }
+
+    const size = await Size.create({
+      name,
+      categoryId,
+      createdBy: id,
+      updatedBy: id,
+    });
+    if (!size) return next(size);
+    return res
+      .status(201)
+      .json(
+        formatResponse(
+          true,
+          201,
+          successMessages(successMessageTypes.createData, 'Size'),
+          { name, categoryId }
+        )
+      );
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { CreateProductSize };
