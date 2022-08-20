4860e2ea6b6fd8a37e68fe0c9debc51e261566b3
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2022-08-21 00:02:52 +0700

    fix: [Products] Add size and category end point for dropdown

--- routes/users/product.route.js
+++ routes/users/product.route.js
@@ -3,8 +3,18 @@ const {
   FindAllProduct,
   FindDetailProduct,
 } = require('../../controllers/products/products');
+const {
+  FindAllProductCategoryForDropDown,
+} = require('../../controllers/products/categories');
+const {
+  FindAllProductSizeForDropDown,
+} = require('../../controllers/products/sizes');
 
 route.get('/', FindAllProduct);
 route.get('/:id', FindDetailProduct);
+
+route.get('/category/dropdown', FindAllProductCategoryForDropDown);
+
+route.get('/size/dropdown', FindAllProductSizeForDropDown);
 
 module.exports = route;
