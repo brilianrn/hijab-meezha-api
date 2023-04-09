420096821dcf549ddfe6c0f2ddc69d2b966b8822
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2023-04-09 15:50:30 +0700

    add log in imagur service

--- services/imgur.service.js
+++ services/imgur.service.js
@@ -1,20 +1,21 @@
-const fs = require('fs');
-const imgur = require('imgur');
-const multer = require('multer');
-const path = require('path');
+const fs = require("fs");
+const imgur = require("imgur");
+const multer = require("multer");
+const path = require("path");
 
 const UploadImage = async (file) => {
   const url = await imgur.uploadFile(`./assets/images/upload/${file.filename}`);
+  console.log(url, " =========== url");
   fs.unlinkSync(`./assets/images/upload/${file.filename}`);
-  return url.data.link;
+  return url?.data?.link;
 };
 
 const Storage = multer.diskStorage({
-  destination: './assets/images/upload',
+  destination: "./assets/images/upload",
   filename: (_, file, callback) => {
     callback(
       null,
-      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
     );
   },
 });
