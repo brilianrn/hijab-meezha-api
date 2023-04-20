f0b3c96b2ef88556c25ddca1ce3b07f1359082f0
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2023-04-20 14:52:08 +0700

    fix size

--- controllers/products/sizes/update.js
+++ controllers/products/sizes/update.js
@@ -1,49 +1,52 @@
-const { errors, successMessageTypes } = require('../../../constants');
-const { Size, Category } = require('../../../models');
-const formatResponse = require('../../../utils/format-response');
-const { successMessages } = require('../../../utils/messages-generate');
-const { UuidCheck } = require('../../../utils/check-fields');
+const { errors, successMessageTypes } = require("../../../constants");
+const { Size, Category } = require("../../../models");
+const formatResponse = require("../../../utils/format-response");
+const { successMessages } = require("../../../utils/messages-generate");
+const { UuidCheck } = require("../../../utils/check-fields");
 
 const UpdateProductSize = async (req, res, next) => {
-  const { name, categoryId } = req.body;
+  const { name, categoryId, isActive } = req.body;
   const { id } = req.params;
 
   if (!name)
     return next({
-      name: errors['400_EMPTY_FIELD'],
-      description: 'Size name',
+      name: errors["400_EMPTY_FIELD"],
+      description: "Size name",
     });
   if (!categoryId)
     return next({
-      name: errors['400_EMPTY_FIELD'],
-      description: 'Category ID',
+      name: errors["400_EMPTY_FIELD"],
+      description: "Category ID",
     });
   if (!UuidCheck(categoryId))
     return next({
-      name: errors['400_WRONG_DATA_TYPE'],
-      description: 'Category ID',
+      name: errors["400_WRONG_DATA_TYPE"],
+      description: "Category ID",
     });
 
   try {
     const category = await Category.findOne({ where: { id: categoryId } });
     if (!category) {
       return next({
-        name: errors['400_NOT_EXIST'],
-        description: 'Category ID',
+        name: errors["400_NOT_EXIST"],
+        description: "Category ID",
       });
     }
 
-    const checkSize = await Size.findOne({ where: { name, categoryId } });
-    if (checkSize) {
-      if (checkSize.name === name && checkSize.categoryId === categoryId) {
-        return next({
-          name: errors['400_EXIST_DATA'],
-          description: 'Size',
-        });
-      }
+    const checkSize = await Size.findOne({ where: { id } });
+    if (!checkSize) {
+      return next({
+        name: errors["404"],
+        description: "Size",
+      });
     }
     const opt = { where: { id } };
-    const payload = { name, categoryId, updatedBy: req.currentAdmin.id };
+    const payload = {
+      name,
+      categoryId,
+      isActive,
+      updatedBy: req.currentAdmin.id,
+    };
     const updateSize = await Size.update(payload, opt);
     if (!updateSize) return next(updateSize);
     return res
@@ -52,7 +55,7 @@ const UpdateProductSize = async (req, res, next) => {
         formatResponse(
           true,
           200,
-          successMessages(successMessageTypes.updateData, 'Product size'),
+          successMessages(successMessageTypes.updateData, "Product size"),
           { name, categoryId }
         )
       );
